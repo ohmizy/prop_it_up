a54f73115a3d3e1a29a5efac861dbe2d1d945036
ohmizy <omarjabr49@yahoo.com>
2022-09-01 16:34:06 -0700

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ class App extends Component {
     return (
         <fieldset>
           <legend>App.jsx</legend>
-          <PersonCard personName = {"Doe, Jane"} age = { "Age: 45" } hairColor = { "Hair Color: Black" } />
-          <PersonCard personName = {"Smith, John"} age = { "Age: 88" } hairColor = { "Hair Color: Brown" } />
-          <PersonCard personName = {"Fillmore, Millard"} age = { "Age: 50" } hairColor = { "Hair Color: Brown" } />
-          <PersonCard personName = {"Smith, Maria"} age = { "Age: 62" } hairColor = { "Hair Color: Brown" } />
+          <PersonCard personName = {"Doe, Jane"} age = { 45 } hairColor = { "Hair Color: Black" } />
+          <PersonCard personName = {"Smith, John"} age = { 88 } hairColor = { "Hair Color: Brown" } />
+          <PersonCard personName = {"Fillmore, Millard"} age = { 50 } hairColor = { "Hair Color: Brown" } />
+          <PersonCard personName = {"Smith, Maria"} age = { 62 } hairColor = { "Hair Color: Brown" } />
         </fieldset>
       
     );
--- src/componets/PersonCard.jsx
+++ src/componets/PersonCard.jsx
@@ -3,15 +3,26 @@ import React, { Component } from 'react'
 export class PersonCard extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      age: this.props.age
+    }
   }
   render() {
-    const { personName, age, hairColor } = this.props;
+    const { personName, hairColor } = this.props;
+    const { age } = this.state;
+    const gettingOld = () => {
+      this.setState({
+        age: age + 1
+      }
+      )
+    }
     return (
       <fieldset>
         <legend>PERSONCARD</legend>
         <h1> { personName } </h1>
-        <h2> { age } </h2>
+        <h2> Age: { age } </h2>
         <h3> { hairColor } </h3>
+        <button onClick = { gettingOld }>Birthday Button for { personName }</button>
       </fieldset>
     );
   }
